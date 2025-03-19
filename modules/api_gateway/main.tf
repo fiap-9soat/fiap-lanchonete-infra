2bb840f31a97ab953355440acb5751606c642e40
daftK <du_ikei@hotmail.com>
2025-03-19 07:35:14 -0300

    test

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -1,3 +1,11 @@
+resource "null_resource" "previous" {}
+
+resource "time_sleep" "wait_1_min" {
+  depends_on = [null_resource.previous]
+
+  create_duration = "60s"
+}
+
 # Deploy the API
 resource "aws_api_gateway_deployment" "api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.fiap_lanchonete_api.id
